b3f24afd316c6842c6067ef7461b518f7a523002
Josh McMillan <josh@joshmcmillan.co.uk>
2020-12-02 01:57:27 +0000

    Split data and meta dictionaries

--- lib/tiers/server.rb
+++ lib/tiers/server.rb
@@ -15,10 +15,12 @@ class Tiers::Server < Sinatra::Base
     restrictions = Tiers::RestrictionLookupService.new(gss_codes: gss_codes).restrictions
 
     {
-      restrictions: restrictions,
-      legal: {
+      data: {
+        restrictions: restrictions
+      },
+      meta: {
         disclaimer: 'This data is provided without warranty and may be inaccurate. Do not depend on it for anything important.',
-        data_source: 'https://github.com/alphagov/collections/blob/2e410b97c64793b853d3947998a1109585f5c3a3/test/fixtures/local-restrictions.yaml'
+        data_source: 'https://github.com/alphagov/collections/blob/2e410b97c64793b853d3947998a1109585f5c3a3/lib/local_restrictions/local-restrictions.yaml'
       }
     }.to_json
   end
